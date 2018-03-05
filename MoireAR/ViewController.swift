e282fb670cef1574e87b391772e9ba66a069bf27
ouchi <ouchi@kobe-du.ac.jp>
2018-03-05 13:04:13 +0900

    Initial Commit

---
+++ MoireAR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoireAR
+//
+//  Created by 大内克哉 on 2018/03/05.
+//  Copyright © 2018年 大内克哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
